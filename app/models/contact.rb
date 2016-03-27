c458c739e6c6c6699aa0b08b12875c5a8272b7d1
Sean Okeefe <sean_o_keefe@hotmail.com>
2016-03-27 18:29:09 +0000

    added contacts controller

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,11 +1,3 @@
-class Contact < ActiveRecord::Migration
-    def change
-     create_table :contacts do |t|
-      t.string :name
-      t.string :email
-      t.text :comments
-      
-      t.timestamps
-     end
-  end
+class Contact < ActiveRecord::Base
+
 end
