cf1cf1e92822878073da61a52d5c46a7203d42fd
Sean Okeefe <sean_o_keefe@hotmail.com>
2016-03-27 18:20:01 +0000

    Added contact form

---
+++ app/models/contact.rb
@@ -0,0 +1,11 @@
+class Contact < ActiveRecord::Migration
+    def change
+     create_table :contacts do |t|
+      t.string :name
+      t.string :email
+      t.text :comments
+      
+      t.timestamps
+     end
+  end
+end
