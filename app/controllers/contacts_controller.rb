df97273ebcb1fb28fba1c00f3ee6d67265855a32
Sean Okeefe <sean_o_keefe@hotmail.com>
2016-06-28 05:33:02 +0000

    TO save modifications to the contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,20 +1,27 @@
 class ContactsController < ApplicationController
-    def new
-        @contact = Contact.new
+  def new
+    @contact = Contact.new
+  end
+  
+  def create
+    @contact = Contact.new(contact_params)
+    
+    if @contact.save
+      name = params[:contact][:name]
+      email = params[:contact][:email]
+      body = params[:contact][:comments]
+      
+      
+      flash[:success] = 'Message sent.'
+      redirect_to new_contact_path
+    else
+      flash[:danger] = 'Error occured, message has not been sent.'
+      redirect_to new_contact_path
     end
-    
-    def create
-        @contact = Contact.new(contact_params)
-    
-        if @contact.save
-            redirect_to new_contact_path, notice: "Message sent."
-        else
-            redirect_to new_contact_path, notice: "Error occured."
-        end
+  end
+  
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
     end
-    
-    private 
-        def contact_params
-            params.require(:contact).permit(:name, :email, :comments)
-        end
 end
